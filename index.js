09a248f6f25ed69c7d07f3100c83a63373296e78
Josemar Luedke <josemarluedke@gmail.com>
2015-01-31 22:40:12 -0800

    Add bootstrap-daterangepicker as dependency and import assets

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-daterangepicker'
+  name: 'ember-cli-daterangepicker',
+
+  included: function(app) {
+    this._super.included(app);
+    this.app.import(app.bowerDirectory + '/moment/moment.js');
+    this.app.import(app.bowerDirectory + '/bootstrap-daterangepicker/daterangepicker.js');
+    this.app.import(app.bowerDirectory + '/bootstrap-daterangepicker/daterangepicker-bs3.css');
+  }
 };
