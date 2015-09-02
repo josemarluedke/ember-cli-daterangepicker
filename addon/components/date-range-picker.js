09e811599c206ad7e66e35b4d36fb9e9a16800f6
Michael Bulpitt <michael@kashoo.co.uk>
2015-09-02 10:14:44 +0100

    null ranges

--- addon/components/date-range-picker.js
+++ addon/components/date-range-picker.js
@@ -28,14 +28,7 @@ export default Ember.Component.extend({
   buttonClasses: ['btn'],
   applyClass: null,
   cancelClass: null,
-  ranges: {
-    'Today': [moment(), moment()],
-    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-    'This Month': [moment().startOf('month'), moment().endOf('month')],
-    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-  },
+  ranges: null,
   removeDropdownOnDestroy: false,
   cancelLabel: 'Cancel',
   applyAction: null,
