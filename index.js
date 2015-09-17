9101bf130374c959d5a8a56be9b1c4a95e4dbea3
SamvelRaja.S <samvelraja.s@zohocorp.com>
2015-09-17 17:56:40 +0530

    Changed the improper CSS import in the index file

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ module.exports = {
     this._super.included(app);
     this.app.import(app.bowerDirectory + '/moment/moment.js');
     this.app.import(app.bowerDirectory + '/bootstrap-daterangepicker/daterangepicker.js');
-    this.app.import(app.bowerDirectory + '/bootstrap-daterangepicker/daterangepicker.css');
+    this.app.import(app.bowerDirectory + '/bootstrap-daterangepicker/daterangepicker-bs3.css');
+    this.app.import(app.bowerDirectory + '/bootstrap-daterangepicker/daterangepicker-bs2.css');
   }
 };
