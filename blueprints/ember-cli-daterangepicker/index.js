7e62d7a1c6c5f09bbab71126ebd83aac8ba22bbd
Joel Sabillon <js1894@yp.com>
2016-03-02 15:13:17 -0800

    Add required dependencies to blueprints afterInstall hook

---
+++ blueprints/ember-cli-daterangepicker/index.js
@@ -0,0 +1,14 @@
+/* jshint node: true */
+
+module.exports = {
+  normalizeEntityName: function() {},
+  
+  afterInstall: function() {
+    return this.addPackagesToProject([
+      { name: 'bootstrap-daterangepicker' },
+      { name: 'ember-cli-moment-shim' },
+      { name: 'moment' },
+      { name: 'moment-timezone' }
+    ]);
+  }
+};
