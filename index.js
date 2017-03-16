b37c960a77b83ee178177513d8823425090404b6
Josemar Luedke <josemarluedke@gmail.com>
2017-03-15 20:59:27 -0700

    Upgrade dependencies and fix deprecation warnings for options.babel

--- index.js
+++ index.js
@@ -11,27 +11,28 @@ module.exports = {
     this._super.included.apply(this, arguments);
 
     if (!process.env.EMBER_CLI_FASTBOOT) {
-      var daterangepickerPath = path.dirname(require.resolve('bootstrap-daterangepicker'));
-      this.options = { daterangepickerPath: daterangepickerPath };
-
-      var vendor = this.treePaths.vendor;
-      this.app.import(vendor + '/bootstrap-daterangepicker/daterangepicker.js');
-      this.app.import(vendor + '/bootstrap-daterangepicker/daterangepicker.css');
+      this.app.import('vendor/bootstrap-daterangepicker/daterangepicker.js');
+      this.app.import('vendor/bootstrap-daterangepicker/daterangepicker.css');
     }
   },
 
   treeForVendor: function(vendorTree) {
     var trees = [];
-    var options = this.options;
+    var daterangepickerPath = path.dirname(require.resolve('bootstrap-daterangepicker'));
 
     if (vendorTree) {
       trees.push(vendorTree);
     }
 
-    trees.push(new Funnel(options.daterangepickerPath, {
+    trees.push(new Funnel(daterangepickerPath, {
       destDir: 'bootstrap-daterangepicker',
       include: [new RegExp(/\.js$|\.css/)],
-      exclude: ['moment', 'moment.min', 'package', 'website'].map(function(key) {
+      exclude: [
+        'moment',
+        'moment.min',
+        'package',
+        'website'
+      ].map(function(key) {
         return new RegExp(key + '\.js$');
       })
     }));
